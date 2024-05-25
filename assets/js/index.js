fdebb04683a553782c796bba856e572ab4717df8
Valentina Sepulveda <vannysepulveda@gmail.com>
2024-05-25 18:18:43 -0400

    modificaciones en el css y html

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,18 @@
 function FunMensaje() {
   window.alert("Correo Enviado")
 }
+
+const triggerTabList = document.querySelectorAll('#myTab button')
+triggerTabList.forEach(triggerEl => {
+  const tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', event => {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
+
+// Initialization for ES Users
+import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple });
